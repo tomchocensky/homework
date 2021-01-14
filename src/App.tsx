79f1e4e4b417024ac978d274bdb0e086ace5fbde
Tomáš Chocenský <tomas.chocensky@seznam.cz>
2021-01-14 18:06:13 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Todo from './Todo';
+
+// rework this into regular api call, feel free to use any open api
+var todos = (): Promise<{id: string; title: string;}[]> => new Promise((res) => {
+  setTimeout(() => {
+    res([
+      {
+        id: "1",
+        title: "Go shopping",
+      },
+      {
+        id: "2",
+        title: "Job interview",
+      },
+      {
+        id: "3",
+        title: "Prepare homework",
+      },
+    ]);
+  }, 1000);
+});
 
 function App() {
+  const [state, setState] = React.useState<{ id: string; title: string }[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      var awaitedTodos = await todos();
+      for (var i = 0; i < awaitedTodos.length; i++) {
+        setState([...state, awaitedTodos[i]]);
+      }
+    })()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {state.map((todo) => (
+        <Todo todo={todo} />
+      ))}
     </div>
   );
 }
---
+++ src/README.md
@@ -0,0 +1,11 @@
+# Homework
+
+Here is sample code that is not by far ideal.
+
+1. Refactor code to production-grade quality.
+2. Find and describe bugs and issues.
+3. Add styling by your choice. (You can add styling library)
+4. Demonstrate connection to backend API.
+5. Add Todo detail page (add routing to app, use context api for state managment from fetch todos to render todos and detail)
+6. Todo component has defined shouldComponentUpdate lifecycle, but it can be done better, adjust it
+7. Optional: rewrite Todo component to FC (choose if you want, prepare explanation)
